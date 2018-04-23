d8db7d31fe42966073b3b8715e8934150bdd34d8
KamLan <adouardcamille@hotmail.com>
2018-04-23 10:59:20 +0200

    modif name file and add save index

--- client-pool.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-echo "scp of the wordpress files to the computer"
-scp -r root@212.47.230.75:/root/save/html /home/kam/Bureau/save-wordpress/
-
-echo "scp of the dump.sql file to the computer"
-scp root@212.47.230.75:/root/save/dump.sql /home/kam/Bureau/save-wordpress/
