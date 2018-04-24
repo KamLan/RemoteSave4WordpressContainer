42f2200345a6374123f388f08cd1d2e20b481c2d
KamLan <adouardcamille@hotmail.com>
2018-04-24 16:31:48 +0200

    modif client-pull.sh

--- client-pull.sh
+++ client-pull.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
 echo "scp of the wordpress files to the computer"
-#scp -r root@212.47.230.75:/root/save/Baptiste/ /home/kam/Courses/Save/Baptiste/
+scp -r root@212.47.230.75:/root/save/Baptiste/ /home/kam/Courses/Save/Baptiste/
 scp -r root@212.47.230.75:/root/save/SDC/ /home/kam/Courses/Save/SDC/
 
